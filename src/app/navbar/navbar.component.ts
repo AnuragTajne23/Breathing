62ca51ff73dc70c29d2d9ec8f8742da7cc1efc21
AnuragTajne23 <anuragtajne73@gmail.com>
2023-04-11 15:52:40 +0530

    update

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  constructor(private router:Router){}
+  logout(){
+    this.router.navigate(['/']);
+    window.localStorage.removeItem('userName');
+   window.localStorage.removeItem('email');
+  }
 }
